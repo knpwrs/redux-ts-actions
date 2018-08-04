dc2f25ec0fb502e882477e650ce8287d9b83174c
Kenneth Powers <ken@kenpowers.net>
2018-08-04 10:36:39 -0400

    feat: more flexible type inference for reduceReducers

--- src/reduce-reducers.test.ts
+++ src/reduce-reducers.test.ts
@@ -2,12 +2,23 @@ import createAction from './create-action';
 import handleAction from './handle-action';
 import reduceReducers from './reduce-reducers';
 
-test('all together', () => {
+test('all together (type handle)', () => {
   const inc = createAction('inc');
   const dec = createAction('dec');
   const re = reduceReducers([
     handleAction<number>(inc, i => i + 1),
     handleAction<number>(dec, i => i - 1),
+  ], 0);
+  const actions = [inc(), dec(), dec(), inc(), inc()];
+  expect(actions.reduce(re, undefined)).toBe(1);
+});
+
+test('all together (type reducer)', () => {
+  const inc = createAction('inc');
+  const dec = createAction('dec');
+  const re = reduceReducers([
+    handleAction(inc, (i: number) => i + 1),
+    handleAction(dec, (i: number) => i - 1),
   ], 0);
   const actions = [inc(), dec(), dec(), inc(), inc()];
   expect(actions.reduce(re, undefined)).toBe(1);
--- src/reduce-reducers.ts
+++ src/reduce-reducers.ts
@@ -1,7 +1,7 @@
 import { Reducer } from 'redux';
 
-export default <S>(res: Reducer[], initialState?: S): Reducer<S, any> => (
-  (state = initialState, action) => (
+export default <S>(res: Reducer<S, any>[], initialState?: S): Reducer<S, any> => (
+  (state = initialState as S, action) => (
     res.reduce(
       (curr, re) => re(curr, action),
       state,
