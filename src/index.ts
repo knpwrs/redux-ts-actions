e78f28c7e9cc83dc30d983f589d8e4736a22f6d5
Kenneth Powers <ken@kenpowers.net>
2018-08-04 13:20:18 -0400

    chore: remove unnecessary eslint comment

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-// eslint-disable-next-line import/prefer-default-export
 export { default as createAction } from './create-action';
 export * from './create-action';
 export { default as handleAction } from './handle-action';
